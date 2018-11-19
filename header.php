cc9eea7e4a6341538b4dff63ee375190550b79e5
lisadebona <annalisa.quiamco@gmail.com>
2018-11-19 20:31:30 +0800

    Added WP Template files

---
+++ header.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The header for theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ACStarter
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+<script defer src="<?php bloginfo( 'template_url' ); ?>/assets/svg-with-js/js/fontawesome-all.js"></script>
+
+
+<?php wp_head(); ?>
+</head>
+<?php
+$logo = get_custom_logo();
+?>
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'acstarter' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="wrapper">
+			
+			<div class="logo">
+				<?php if($logo) { ?>
+					<?php echo $logo; ?>
+				<?php } else { ?>
+					<h2 class="logo-name"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h2>
+				 <?php } ?>	
+			</div>
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'acstarter' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+	</div><!-- wrapper -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content wrapper">
